8360b267925311b0c0874f925b6bddd4b5ecc8d2
pmx <mingl0280@gmail.com>
2016-10-18 01:16:12 -0600

    initial submit
    Signed-off-by: pmx <mingl0280@gmail.com>

---
+++ wbp-rev/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 12)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "目录"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(47, 6)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(601, 21)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(654, 4)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "brownse"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(71, 68)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "fuck"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(573, 68)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "quit"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 12)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "From ver"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(321, 44)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(35, 12)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "ToVer"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(71, 41)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(244, 21)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(362, 41)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(286, 21)
+        Me.TextBox3.TabIndex = 8
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(14, 97)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(634, 388)
+        Me.ListBox1.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(741, 499)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "fuck wbp"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+End Class
---
+++ wbp-rev/Form1.vb
@@ -0,0 +1,75 @@
+﻿Imports Microsoft.Win32
+Imports System.IO
+Imports System.Threading
+Imports System.Xml
+
+Public Class Form1
+
+    Private Delegate Sub addNewItem(ByRef i As String)
+    Private Delegate Sub updateItem(ByRef i As String, ByRef j As String)
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim foldbox As New FolderBrowserDialog
+        Dim ret = foldbox.ShowDialog()
+        If ret = DialogResult.OK Then
+            TextBox1.Text = foldbox.SelectedPath
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim rValue = Registry.ClassesRoot.OpenSubKey("wbpfile").OpenSubKey("Shell").OpenSubKey("Open").OpenSubKey("Command")
+        Dim text As String = DirectCast(rValue.GetValue(""), String)
+        TextBox1.Text = text.Replace("""", "").Replace("%1", "").Replace("WBPInstaller.exe", "").Trim + "WBP\"
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim th As New Thread(AddressOf nThread)
+        th.Start()
+    End Sub
+
+    Private Sub addListItem(ByRef i As String)
+        ListBox1.Items.Add(i.Clone())
+    End Sub
+    Private Sub updateListItem(ByRef i As String, ByRef j As String)
+        Dim idx = ListBox1.Items.IndexOf(i)
+        ListBox1.SelectedIndex = idx
+        ListBox1.Items(ListBox1.SelectedIndex) = j
+    End Sub
+
+    Private Sub nThread()
+        Dim filelist As New List(Of File)
+        Dim FI As FileInfo()
+        Dim DI = New DirectoryInfo(TextBox1.Text)
+        Dim procStr As String = ""
+        Dim fkstr As String = ""
+        FI = DI.GetFiles("*.wbp")
+        For Each f As FileInfo In FI
+            procStr = "Fucking "
+            Using zipToOpen = New FileStream(f.FullName, FileMode.Open)
+                Using archive = New ZipArchive(zipToOpen, ZipArchiveMode.Update)
+                    Using tStream As Stream = archive.GetEntry("wbpinfo").Open()
+                        procStr += f.FullName + "..."
+                        fkstr = procStr
+                        BeginInvoke(New addNewItem(AddressOf addListItem), procStr)
+                        Debug.WriteLine(fkstr)
+                        Dim AnalyzeXML = New XmlDocument
+                        AnalyzeXML.Load(tStream)
+                        tStream.Seek(0, 0)
+                        Dim tval = AnalyzeXML.SelectSingleNode("/Package/Version").InnerText
+                        If tval = TextBox2.Text Then
+                            AnalyzeXML.SelectSingleNode("/Package/Version").InnerText = TextBox3.Text
+                            AnalyzeXML.Save(tStream)
+                            procStr += "fucked."
+                        Else
+                            procStr += "not fucked."
+                        End If
+                        BeginInvoke(New updateItem(AddressOf updateListItem), fkstr, procStr)
+                        Debug.WriteLine(procStr)
+                    End Using
+                End Using
+            End Using
+        Next
+    End Sub
+End Class
